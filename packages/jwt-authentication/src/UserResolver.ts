32199eecbba65ad7b52e786f1d3a0b2cde975b0b
Terry Tilley <terry@terrytilley.com>
2019-10-14 23:45:34 +0100

    Register mutation

--- packages/jwt-authentication/src/UserResolver.ts
+++ packages/jwt-authentication/src/UserResolver.ts
@@ -1,9 +1,32 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { hash } from "bcryptjs";
+import { User } from "./entity/User";
 
 @Resolver()
 export class UserResolver {
-  @Query(() => String)
-  hello() {
-    return "Hello world!!!";
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const salt = 12;
+    const hashedPassword = await hash(password, salt);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword
+      });
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+
+    return true;
   }
 }
--- packages/jwt-authentication/src/index.ts
+++ packages/jwt-authentication/src/index.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import express from "express";
-import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 
 (async () => {
   const port = 4000;
   const app = express();
+
+  await createConnection();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
@@ -20,21 +23,3 @@ import { UserResolver } from "./UserResolver";
     console.log(`🚀 Server running at http://localhost:${port}`);
   });
 })();
-
-// createConnection()
-//   .then(async connection => {
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-//   })
-//   .catch(error => console.log(error));

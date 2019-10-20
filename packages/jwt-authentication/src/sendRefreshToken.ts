c1724cefac8fac8ee6b89be234492992e436d1bb
Terry Tilley <terry@terrytilley.com>
2019-10-20 16:07:23 +0100

    removed token path for nextjs

--- packages/jwt-authentication/src/sendRefreshToken.ts
+++ packages/jwt-authentication/src/sendRefreshToken.ts
@@ -3,7 +3,8 @@ import { Response } from "express";
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("jid", token, {
     httpOnly: true,
-    path: "/refresh_token",
+    // domain: '.example.com',
+    // path: "/refresh_token", // Set path if not server sider rendering
     expires: new Date(Date.now() + 168 * 3600000) // cookie will be removed after 7 days
   });
 };

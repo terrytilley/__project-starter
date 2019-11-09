dc666667ec4e19d4c2585db476916bfd7bbcefc7
Terry Tilley <terry@terrytilley.com>
2019-11-09 21:54:24 +0000

    added material ui

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,16 +1,36 @@
 import { ApolloProvider } from '@apollo/react-hooks';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 
 import { withApollo } from '../lib/apollo';
+import theme from '../theme';
 
 class MyApp extends App<any> {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps, apolloClient } = this.props;
 
     return (
       <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
+        <React.Fragment>
+          <Head>
+            <title>Project Starter</title>
+          </Head>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </React.Fragment>
       </ApolloProvider>
     );
   }

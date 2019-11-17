c3dd9574629c0b57cffcb96b51c30850f370fe16
Terry Tilley <terry@terrytilley.com>
2019-11-17 18:21:21 +0000

    resetPassword mutation

--- packages/server/src/graphql/resolvers/Auth.ts
+++ packages/server/src/graphql/resolvers/Auth.ts
@@ -1,3 +1,4 @@
+import { verify } from 'jsonwebtoken';
 import { Arg, Ctx, Mutation } from 'type-graphql';
 
 import { User } from '../../entity/User';
@@ -77,5 +78,39 @@ export class AuthResolver {
     user.save();
 
     return response;
+  }
+
+  @Mutation(() => Boolean)
+  async resetPassword(
+    @Arg('token', () => String) token: string,
+    @Arg('userId', () => String) userId: string,
+    @Arg('newPassword', () => String) newPassword: string
+  ) {
+    const user = await User.findOne({ id: userId });
+
+    if (!user) return false;
+
+    let payload: any = null;
+    const secret = `${user.password}-${user.createdAt}`;
+
+    try {
+      payload = verify(token, secret);
+    } catch (err) {
+      console.error(err);
+
+      return false;
+    }
+
+    if (payload.userId === user.id) {
+      const hashedPassword = await User.hashPassword(newPassword);
+
+      user.locked = false;
+      user.password = hashedPassword;
+      user.save();
+
+      return true;
+    }
+
+    return false;
   }
 }

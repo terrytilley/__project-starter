fa619550b2b6856c93dd84afacf8fd59b2d1cba7
Terry Tilley <terry@terrytilley.com>
2019-11-18 17:57:07 +0000

    password reset page

--- packages/web/generated/graphql.tsx
+++ packages/web/generated/graphql.tsx
@@ -106,6 +106,17 @@ export type RegisterMutation = { __typename?: 'Mutation' } & {
   register: { __typename?: 'User' } & Pick<User, 'id' | 'email' | 'createdAt'>;
 };
 
+export type ResetPasswordMutationVariables = {
+  newPassword: Scalars['String'];
+  userId: Scalars['String'];
+  token: Scalars['String'];
+};
+
+export type ResetPasswordMutation = { __typename?: 'Mutation' } & Pick<
+  Mutation,
+  'resetPassword'
+>;
+
 export type UsersQueryVariables = {};
 
 export type UsersQuery = { __typename?: 'Query' } & {
@@ -388,6 +399,54 @@ export type RegisterMutationResult = ApolloReactCommon.MutationResult<RegisterMu
 export type RegisterMutationOptions = ApolloReactCommon.BaseMutationOptions<
   RegisterMutation,
   RegisterMutationVariables
+>;
+export const ResetPasswordDocument = gql`
+  mutation ResetPassword($newPassword: String!, $userId: String!, $token: String!) {
+    resetPassword(newPassword: $newPassword, userId: $userId, token: $token)
+  }
+`;
+export type ResetPasswordMutationFn = ApolloReactCommon.MutationFunction<
+  ResetPasswordMutation,
+  ResetPasswordMutationVariables
+>;
+
+/**
+ * __useResetPasswordMutation__
+ *
+ * To run a mutation, you first call `useResetPasswordMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useResetPasswordMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [resetPasswordMutation, { data, loading, error }] = useResetPasswordMutation({
+ *   variables: {
+ *      newPassword: // value for 'newPassword'
+ *      userId: // value for 'userId'
+ *      token: // value for 'token'
+ *   },
+ * });
+ */
+export function useResetPasswordMutation(
+  baseOptions?: ApolloReactHooks.MutationHookOptions<
+    ResetPasswordMutation,
+    ResetPasswordMutationVariables
+  >
+) {
+  return ApolloReactHooks.useMutation<
+    ResetPasswordMutation,
+    ResetPasswordMutationVariables
+  >(ResetPasswordDocument, baseOptions);
+}
+export type ResetPasswordMutationHookResult = ReturnType<typeof useResetPasswordMutation>;
+export type ResetPasswordMutationResult = ApolloReactCommon.MutationResult<
+  ResetPasswordMutation
+>;
+export type ResetPasswordMutationOptions = ApolloReactCommon.BaseMutationOptions<
+  ResetPasswordMutation,
+  ResetPasswordMutationVariables
 >;
 export const UsersDocument = gql`
   query Users {
---
+++ packages/web/pages/password/reset/[userId]/[token]/index.tsx
@@ -0,0 +1,147 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Snackbar,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { Field, Form, Formik, FormikProps } from 'formik';
+import Router, { useRouter } from 'next/router';
+import React from 'react';
+
+import Copyright from '../../../../../components/Copyright';
+import SnackbarContentWrapper from '../../../../../components/SnackbarContentWrapper';
+import { useResetPasswordMutation } from '../../../../../generated/graphql';
+import Layout from '../../../../../layouts/Main';
+
+export default function PasswordResetPage() {
+  const router = useRouter();
+
+  interface FormValues {
+    newPassword: string;
+  }
+
+  const [state, setState] = React.useState({
+    open: false,
+    message: '',
+  });
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
+  const [resetPassword] = useResetPasswordMutation();
+
+  const onSubmit = async (
+    { newPassword }: FormValues,
+    { setSubmitting, resetForm }: any
+  ) => {
+    setSubmitting(true);
+
+    const { userId, token } = router.query;
+    const response = await resetPassword({
+      variables: {
+        newPassword,
+        userId: userId as string,
+        token: token as string,
+      },
+    });
+
+    if (response && response.data && response.data.resetPassword) {
+      Router.push('/login');
+    } else {
+      setState({ ...state, message: 'Failed to reset password', open: true });
+    }
+
+    setSubmitting(false);
+    resetForm();
+  };
+
+  const useStyles = makeStyles(theme => ({
+    '@global': {
+      body: {
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <Layout>
+      <Container component="main" maxWidth="xs">
+        <Formik initialValues={{ newPassword: '' }} onSubmit={onSubmit}>
+          {({ isSubmitting }: FormikProps<FormValues>) => (
+            <div className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Reset Password
+              </Typography>
+              <Form className={classes.form} noValidate={true}>
+                <Field
+                  type="password"
+                  name="newPassword"
+                  label="New password"
+                  margin="normal"
+                  variant="outlined"
+                  fullWidth={true}
+                  required={true}
+                  autoFocus={true}
+                  as={TextField}
+                />
+                <Button
+                  type="submit"
+                  color="primary"
+                  variant="contained"
+                  fullWidth={true}
+                  disabled={isSubmitting}
+                  className={classes.submit}
+                >
+                  Reset
+                </Button>
+              </Form>
+              <Snackbar
+                open={state.open}
+                onClose={handleClose}
+                autoHideDuration={3000}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+              >
+                <SnackbarContentWrapper
+                  variant="error"
+                  message={state.message}
+                  onClose={handleClose}
+                />
+              </Snackbar>
+            </div>
+          )}
+        </Formik>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </Layout>
+  );
+}

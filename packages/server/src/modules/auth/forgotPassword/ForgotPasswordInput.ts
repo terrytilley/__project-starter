449087cdc55266bc8279306ecf6d5e24253f96f9
Terry Tilley <terry@terrytilley.com>
2019-12-01 01:38:55 +0000

    forgot password and login validation

--- packages/server/src/modules/auth/forgotPassword/ForgotPasswordInput.ts
+++ packages/server/src/modules/auth/forgotPassword/ForgotPasswordInput.ts
@@ -1,8 +1,10 @@
 import { IsEmail } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 
+import { User } from '../../../entity/User';
+
 @InputType()
-export class ForgotPasswordInput {
+export class ForgotPasswordInput implements Partial<User> {
   @Field()
   @IsEmail()
   email: string;
--- packages/server/src/modules/auth/login/LoginInput.ts
+++ packages/server/src/modules/auth/login/LoginInput.ts
@@ -1,8 +1,10 @@
 import { IsEmail, Length } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
 
+import { User } from '../../../entity/User';
+
 @InputType()
-export class LoginInput {
+export class LoginInput implements Partial<User> {
   @Field()
   @IsEmail()
   email: string;
